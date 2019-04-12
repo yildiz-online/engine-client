34f297dd06c16fa505a16e1bad7563df9b9291c3
Grégory Van den Borre <vandenborre.gregory@hotmail.fr>
2019-04-12 09:24:40 +0200

    [WIP] Fix tests.

--- src/test/java/be/yildizgames/engine/client/GameEngineFactoryTest.java
+++ src/test/java/be/yildizgames/engine/client/GameEngineFactoryTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.Test;
 public class GameEngineFactoryTest {
 
     @Nested
-    class Build {
+    public class Build {
 
         @Test
         public void happyFlow() {
